63d5d25dd7accf02d04c0f8c4dd59d75d47820a1
rprabhuh <rprabhu@ufl.edu>
2015-09-11 00:12:43 -0400

    I've added boiler plate code for the project. This compiles nonetheless.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,24 +1,69 @@
-import akka.actor.Actor
-import akka.actor.ActorSystem
+import akka.actor._
 import akka.actor.Props
- 
-class HelloActor extends Actor {
+import akka.routing.RoundRobinRouter
+import scala.concurrent.duration.Duration
+
+//Create the base trait for messages
+sealed trait bitCoin
+
+//Create the Messages
+class start extends bitCoin
+
+
+//Create the worker
+class Worker extends Actor{
   def receive = {
-    case "hello" => println("hello back at you")
-    case _       => println("huh?")
+    /*
+     * case <messagetype> => some action
+     */
+    case start =>
+      println("Started Code");
   }
-}
- 
-object Main extends App {
-  val system = ActorSystem("HelloSystem")
-  // default Actor constructor
-  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
-  helloActor ! "hello"
-  helloActor ! "buenos dias"
+
 }
 
-/*
-object Hi {
-  def main(args: Array[String]) = println("Hi!")
+//Create the master 
+class Master(numWorkers: Int, numMessages: Int, nrOfElements: Int, numZeros: Int, listener: ActorRef) extends Actor {
+
+  //Initialize stuff
+  
+  
+  //Create the scheduling algorithm
+  val workerRouter = context.actorOf(
+  Props[Worker].withRouter(RoundRobinRouter(numWorkers)), name = "workerRouter")
+
+
+  def receive = {
+    /*
+     * case <messagetype> => some action
+     *
+     * case Result =>
+     * Do a bunch of things and send a message to the listener
+     */
+    case start =>
+      println("Started Code");
+  }
+
+
 }
-*/
+
+//Create the result listener
+class Listener extends Actor {
+  def receive = {
+    case start =>
+      println("Got result");
+  }
+}
+
+
+//Create the App
+object Main extends App {
+  //Create the listener
+ 
+  
+  //Create the Master
+  
+  
+  //Send the master the start message
+  
+}
